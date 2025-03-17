e1b84000a9ce7fe96ea9b3f75f2d7ec24559ed94
kahkashan436 <simrankahkashan08@gmail.com>
2025-03-17 23:50:59 +0530

    Update Test2Application.java

--- src/main/java/com/test2/Test2Application.java
+++ src/main/java/com/test2/Test2Application.java
@@ -7,11 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test2Application {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
                 int x=100;
-=======
 		String name="mike";
->>>>>>> 25a1e3e4bbe97fa6afe97ad17c53d1abe03ecee5
 		SpringApplication.run(Test2Application.class, args);
 	}
 
